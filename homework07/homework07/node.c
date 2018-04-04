6caaa71f7e01e9ea3964d02859875861143e4793
Ryan Karl <rkarl@nd.edu>
2018-04-04 19:07:40 -0400

    homework07: node

--- homework07/homework07/node.c
+++ homework07/homework07/node.c
@@ -17,7 +17,14 @@ void    node_set_symbol(Node *n, int64_t letter);
  * @return  Newly allocated Node structure with specified attributes.
  */
 Node *  node_create(int64_t symbol, size_t count, Node *left, Node *right) {
-    return NULL;
+    Node *n = malloc(sizeof(Node));
+    if(n){
+        node_set_symbol(n, symbol);
+        n->count = count;
+        n->left = left;
+        n->right = right;
+}
+return n;
 }
 
 /**
@@ -26,6 +33,13 @@ Node *  node_create(int64_t symbol, size_t count, Node *left, Node *right) {
  * @return  NULL.
  */
 Node *  node_delete(Node *n) {
+   
+    if(n){
+         node_delete(n->left);
+         node_delete(n->right);
+         free(n);
+            
+    }
     return NULL;
 }
 
@@ -35,6 +49,9 @@ Node *  node_delete(Node *n) {
  * @param   stream      I/O to write to.
  */
 void    node_dump(const Node *n, FILE *stream) {
+    
+    fprintf(stream, "Node{symbol: %s, count: %lu, left: 0x%lX, right: 0x%lX}\n", n->symbol.string, n->count, (int64_t) n->left, (int64_t) n->right);
+    
 }
 
 /* Internal Node Functions */
@@ -44,7 +61,52 @@ void    node_dump(const Node *n, FILE *stream) {
  * @param   n           Node structure.
  * @param   letter      Node symbol letter value.
  */
-void    node_set_symbol(Node *n, int64_t letter) {                         
+void    node_set_symbol(Node *n, int64_t letter){
+    
+    n->symbol.letter=letter;
+
+    switch((char) letter){
+        case '\n':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = 'n';
+            n->symbol.string[2] = '\0';
+            break;
+        case '\"':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = '"';
+            n->symbol.string[2] = '\0';
+            break;
+        case '\\':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = '\\';
+            n->symbol.string[2] = '\0';
+            break;
+        case '\b':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = 'b';
+            n->symbol.string[2] = '\0';
+            break;
+        case '\f':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = 'f';
+            n->symbol.string[2] = '\0';
+            break;
+        case '\r':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = 'r';
+            n->symbol.string[2] = '\0';
+            break;
+        case '\t':
+            n->symbol.string[0] = '\\';
+            n->symbol.string[1] = 't';
+            n->symbol.string[2] = '\0';
+            break;
+        default:
+            n->symbol.string[0] = (char) letter;
+            n->symbol.string[1] = '\0';
+            break;    
+
+    }
 }                                                            
 
 /* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
