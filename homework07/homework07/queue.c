481f2f8ea4e5a9710553eea6edc2ed50d8027d18
Ryan Karl <rkarl@nd.edu>
2018-04-04 19:08:03 -0400

    homework07: queue

--- homework07/homework07/queue.c
+++ homework07/homework07/queue.c
@@ -18,7 +18,22 @@ size_t  queue_minimum_child(Queue *q, size_t i);
  * @return  Newly allocated Queue structure with specified capacity.
  */
 Queue * queue_create(size_t capacity) {
-    return NULL;
+    if(!capacity) {
+        capacity = PRIORITY_QUEUE_CAPACITY;
+    }
+    Queue *q = calloc(1, sizeof(Queue));   
+        if (q){
+            q->size = 0;
+            q->nodes = calloc(capacity, sizeof(Node*));
+            if(!q->nodes){
+                free(q);
+                return NULL;
+            }
+            
+            q->capacity = capacity;
+         }
+
+    return q;
 }
 
 /**
@@ -27,7 +42,11 @@ Queue * queue_create(size_t capacity) {
  * @return  NULL.
  */
 Queue * queue_delete(Queue *q) {
-    return NULL;
+    
+    free(q->nodes);
+    free(q);
+    q = NULL;
+    return q;
 }
 
 /**
@@ -37,7 +56,25 @@ Queue * queue_delete(Queue *q) {
  * @return  Whether or not the operation was successful.
  */
 bool    queue_push(Queue *q, Node *n) {
-    return false;
+    
+    if(n == NULL){
+        return false;
+    }
+    if(q == NULL){
+        return false;
+    }
+    if(q->size >= q->capacity){
+        if(queue_resize(q, 2 * (q->capacity)) == false){
+            
+            return false;
+        }
+    }
+            
+    q->nodes[q->size] = n;
+    queue_bubble_up(q, q->size);
+    q->size += 1;
+    
+    return true;
 }
 
 /**
@@ -46,7 +83,17 @@ bool    queue_push(Queue *q, Node *n) {
  * @return  Minimum Node structure (by count) or NULL.
  */
 Node *	queue_pop(Queue *q) {
-    return NULL;
+    
+    if(q->size==0){
+        return NULL;
+    }
+
+    Node * result = q->nodes[0];
+    q->size-=1;
+    q->nodes[0] = q->nodes[q->size];
+    q->nodes[q->size] = NULL;
+    queue_bubble_down(q,0);
+    return result;
 }
 
 /**
@@ -55,6 +102,14 @@ Node *	queue_pop(Queue *q) {
  * @param   stream      I/O stream to write to.
  */
 void    queue_dump(const Queue *q, FILE *stream) {
+
+    if(q->nodes == NULL){
+        return;
+    }
+    for(int i = 0; i < q->size; i++){
+        node_dump(q->nodes[i], stream);
+    }
+
 }
 
 /* Internal Priority Queue Functions */
@@ -66,7 +121,11 @@ void    queue_dump(const Queue *q, FILE *stream) {
  * @return  Whether or not operation succeeded.
  */
 bool    queue_resize(Queue *q, size_t capacity) {
-    return false;
+    
+    q->capacity = capacity;
+    q->nodes = realloc(q->nodes, q->capacity*sizeof(Node*));
+
+    return q->nodes;
 }
 
 /**
@@ -75,6 +134,17 @@ bool    queue_resize(Queue *q, size_t capacity) {
  * @param   i           Index to current Node structure.
  */
 void    queue_bubble_up(Queue *q, size_t i) {
+
+    while(i > 0) 
+    {
+        size_t parent = PARENT(i);
+        if((q->nodes[i])->count < (q->nodes[parent])->count){
+            Node * temp = q->nodes[i];
+            q->nodes[i] = q->nodes[parent];
+            q->nodes[parent] = temp;
+        }
+            i = parent;    
+    }
 }
 
 /**
@@ -83,6 +153,18 @@ void    queue_bubble_up(Queue *q, size_t i) {
  * @param   i           Index to current Node structure.
  */
 void    queue_bubble_down(Queue *q, size_t i) {
+
+    Node * temp;
+    while(LEFT_CHILD(i) < q->size)
+    {
+        size_t min = queue_minimum_child(q, i);
+        if(q->nodes[min]->count < (q->nodes[i])->count){
+            temp = q->nodes[i];
+            q->nodes[i] = q->nodes[min];
+            q->nodes[min] = temp;
+        }
+    i = min;
+    }
 }
 
 /**
@@ -91,7 +173,20 @@ void    queue_bubble_down(Queue *q, size_t i) {
  * @param   i           Index to current Node structure.
  */
 size_t  queue_minimum_child(Queue *q, size_t i) {
-    return 0;
+    
+    size_t l = LEFT_CHILD(i);
+    size_t r = RIGHT_CHILD(i);
+
+    if(!q->nodes[r])
+    {
+        return l;
+    }
+
+    if(q->nodes[l]->count < q->nodes[r]->count){
+        return l;
+    }
+
+    return r;
 }
 
 /* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
